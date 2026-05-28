fe499b851ac8676930963d79326c077899c43739
sat7312 <sat7312@gmail.com>
2026-05-28 09:02:56 +0900

    초기 세팅

---
+++ src/main/kotlin/com/example/demo/HomeController.kt
@@ -0,0 +1,13 @@
+package com.example.demo
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    fun main(): String {
+        return "Hi, SpringBoot!!"
+    }
+}
